4820a75feabb29708ca9216dea5f960b190f8576
kuyol-project <153427860+BUILDPROJECT222@users.noreply.github.com>
2024-05-26 17:37:07 +0700

    Delete settings.tsx

--- settings.tsx
+++
@@ -1,3 +0,0 @@
-export const mintText = [
-  { label: "OGs", mintText: "WL mint paying with SOL", buttonLabel: "Mint now!", header: "WL Mint" },
-  { label: "Public", mintText: "Mint Paying with SOL", buttonLabel: "Mint now!", header: "Public Mint" },
