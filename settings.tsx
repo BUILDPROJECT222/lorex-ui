2b1217d371179d5467675f08237afe58500cc492
kuyol-project <153427860+BUILDPROJECT222@users.noreply.github.com>
2024-05-26 12:41:53 +0700

    Create settings.tsx

---
+++ settings.tsx
@@ -0,0 +1,3 @@
+export const mintText = [
+  { label: "OGs", mintText: "WL mint paying with SOL", buttonLabel: "Mint now!", header: "WL Mint" },
+  { label: "Public", mintText: "Mint Paying with SOL", buttonLabel: "Mint now!", header: "Public Mint" },
